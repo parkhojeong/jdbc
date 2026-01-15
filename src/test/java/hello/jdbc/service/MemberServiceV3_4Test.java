71e7938f5149a3d27f95c5a41b5d522c3eda1c59
parkhojeong <parkhj062@gmail.com>
2026-01-16 00:40:39 +0900

    [40. 스프링 부트의 자동 리소스 등록]

---
+++ src/test/java/hello/jdbc/service/MemberServiceV3_4Test.java
@@ -0,0 +1,103 @@
+package hello.jdbc.service;
+
+import hello.jdbc.connection.ConnectionConst;
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV3;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@Slf4j
+@SpringBootTest
+class MemberServiceV3_4Test {
+
+    public static final String MEMBER_A = "memberA";
+    public static final String MEMBER_B = "memberB";
+    public static final String MEMBER_EX = "ex";
+
+    @Autowired
+    private MemberRepositoryV3 memberRepository;
+    @Autowired
+    private MemberServiceV3_3 memberService;
+
+    @TestConfiguration
+    static class TestConfig {
+
+        private final DataSource dataSource;
+
+        TestConfig(DataSource dataSource) {
+            this.dataSource = dataSource;
+        }
+
+        @Bean
+        MemberServiceV3_3 memberService(MemberRepositoryV3 memberRepository) {
+            return new MemberServiceV3_3(memberRepository);
+        }
+
+        @Bean
+        MemberRepositoryV3 memberRepository() {
+            return new MemberRepositoryV3(dataSource);
+        }
+    }
+
+    @AfterEach
+    void tearDown() throws SQLException {
+        memberRepository.deleteById(MEMBER_A);
+        memberRepository.deleteById(MEMBER_B);
+        memberRepository.deleteById(MEMBER_EX);
+    }
+
+    @Test
+    void AopCheck() {
+        log.info("memberService = {}", memberService.getClass());
+        log.info("memberRepository = {}", memberRepository.getClass());
+        Assertions.assertThat(AopUtils.isAopProxy(memberService)).isTrue();
+        Assertions.assertThat(AopUtils.isAopProxy(memberRepository)).isFalse();
+    }
+
+    @Test
+    void accountTransferNormal() throws SQLException {
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberB = new Member(MEMBER_B, 10000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberB);
+
+        log.info("START TX");
+        memberService.accountTransfer(memberA.getMemberId(), memberB.getMemberId(), 1000);
+        log.info("END TX");
+
+        Member memberA1 = memberRepository.findById(memberA.getMemberId());
+        Member memberB1 = memberRepository.findById(memberB.getMemberId());
+        Assertions.assertThat(memberA1.getMoney()).isEqualTo(9000);
+        Assertions.assertThat(memberB1.getMoney()).isEqualTo(11000);
+    }
+
+    @Test
+    void accountTransferException() throws SQLException {
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberEx = new Member(MEMBER_EX, 10000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberEx);
+
+        Assertions.assertThatThrownBy(() -> memberService.accountTransfer(memberA.getMemberId(), memberEx.getMemberId(), 1000))
+                .isInstanceOf(IllegalStateException.class);
+
+        Member memberA1 = memberRepository.findById(memberA.getMemberId());
+        Member memberB1 = memberRepository.findById(memberEx.getMemberId());
+        Assertions.assertThat(memberA1.getMoney()).isEqualTo(10000);
+        Assertions.assertThat(memberB1.getMoney()).isEqualTo(10000);
+
+    }
+}
